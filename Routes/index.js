06b2d91d35d6b26011a7922c175abcf65f0e752b
Brent Graves <graves.brent@gmail.com>
2021-11-14 16:26:11 -0500

    fitness tracker is seeding and adding to db from localhost page. adding comments on functionality.

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,8 @@
+// require express apiRoutes and htmlRoutes
 const router = require("express").Router();
 const apiRoutes = require("./apiRoutes");
 const htmlRoutes = require("./htmlRoutes");
-
+//use apiRoutes and htmlRoutes
 router.use("/apiRoutes", apiRoutes);
 router.use("/htmlRoutes", htmlRoutes);
 
