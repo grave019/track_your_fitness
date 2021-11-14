e4844956f69734a4681092624a9dd626d746d032
Brent Graves <graves.brent@gmail.com>
2021-11-14 15:13:19 -0500

    added router.get to apiRoutes.

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const { route } = require("./htmlRoutes");
-
+//router post
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create({ body })
     .then((dbWorkouts) => {
@@ -11,11 +11,33 @@ router.post("/api/workouts", ({ body }, res) => {
       res.status(400).json(err);
     });
 });
-
+//router delete
 router.delete("/api/workouts", ({ body }, res) => {
     Workout.destroy(body)
       .then((dbWorkouts) => {
         res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+//router get
+  router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+          totalWeight: {
+            $sum: "$exercises.weight",
+          },
+        },
+      },
+    ])
+      .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+        console.log(dbWorkouts);
       })
       .catch((err) => {
         res.status(400).json(err);
